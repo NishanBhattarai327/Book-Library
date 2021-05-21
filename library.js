abae87111a303d399f95c89323c82f5ead57d344
NishanBhattarai327 <bhattarainishan327@gmail.com>
2021-05-21 12:19:27 +0545

    Create a function to render book objects for library

--- library.js
+++ library.js
@@ -1,7 +1,30 @@
 //all of book objs will be stored in this arrray
-let library = [];
+let library = [
+	{title: 'Harry Potter', author: 'J K Rowely', pages: 560, read: false},
+	{title: 'Fantastic Beast', author: 'J K Rowely', pages: 560, read: false}
+];
+
+let $displayBook = document.querySelector('#displayBook');
+
 
 //this function will add book objs to library
 function addBook(title, author, pages, read) {
 	library.push({title, author, pages, read});
 }
+
+function renderBook() {
+
+	library.forEach((book) => {
+		let $book = document.createElement('div');
+		$book.setAttribute('class', 'book');
+
+		for (let property in book) {
+			let $property = document.createElement('div');
+			$property.setAttribute('class', `bookProperty ${property}`);
+			$property.innerHTML = `<div class='bookContent title'>${property}</div> <div class='bookContent value'>${book[property]}</div><br>`;
+			$book.appendChild($property);
+		}
+		$displayBook.appendChild($book); 
+	});
+}
+renderBook();
