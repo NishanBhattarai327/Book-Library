dabbb8485041417ca971207ece3b106aac754c6d
NishanBhattarai327 <bhattarainishan327@gmail.com>
2021-05-21 11:25:32 +0545

    Create function to add book objects to library array

--- library.js
+++ library.js
@@ -1,2 +1,7 @@
 //all of book objs will be stored in this arrray
 let library = [];
+
+//this function will add book objs to library
+function addBook(title, author, pages, read) {
+	library.push({title, author, pages, read});
+}
